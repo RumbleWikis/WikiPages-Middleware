a00bc7251c7be853d4bf1a84e4811f45c7dd94ae
Julli4n <11952329+Julli4n@users.noreply.github.com>
2021-07-19 10:24:58 -0700

    add wikipages

--- middleware/deno-bundler/index.ts
+++ middleware/deno-bundler/index.ts
@@ -7,65 +7,7 @@ import babelPluginProposalOptionalChaining from "@babel/plugin-proposal-optional
 // @ts-ignore: Ignore any
 import babelPresetEnv from "@babel/preset-env";
 
-/**
- * A file interface for WikiPages.
- */
-interface WPFile {
-  /**
-   * The original directory of the file.
-   */
-  readonly originalDirectory: string;
-  /**
-   * The short extension of the file, ex `.lua`, not including `.client.lua`
-   */
-  shortExtension: string;
-  /**
-   * The long extension o fthe file, ex `.client.lua`, not including **just** `.lua`
-   */
-  longExtension: string;
-  /**
-   * The comment for the MediaWiki commit
-   */
-  commitComment: string;
-  /**
-   * Whether or not it should commit when it goes through all middlewares
-   */
-  shouldCommit: boolean;
-  /**
-   * THe new MediaWiki path of the file.
-   */
-  path: string;
-  /**
-   * The source string of the file.
-   */
-  source: string;
-}
-
-/**
- * Middleware that will be called on every file found.
- */
-interface Middleware {
-  /**
-   * The RegExp/string to match in the short extension,  ex `.lua`, not including `.client.lua`
-   */
-  matchShortExtension?: RegExp | string;
-  /**
-   * The RegExp/string to match in the long extension, ex `.client.lua`, not including **just** `.lua`
-   */
-  matchLongExtension?: RegExp | string;
-  /**
-   * The Regexp/string to match in the path of the file
-   */
-  matchPath?: RegExp | string;
-  /**
-   * The settings index to be indexed with
-   */
-  settingsIndex?: string;
-  /**
-   * The execute function of the middleware
-   */
-  execute: (file: WPFile, settings?: Record<string, unknown>) => WPFile
-}
+import type { Middleware } from "@rumblewikis/wikipages";
 
 /**
  * Settings for Deno Bundler.
@@ -80,6 +22,7 @@ interface DenoBundlerSettings {
    */
   useBabel?: boolean;
 }
+
 
 const denoBundler: Middleware = {
   matchShortExtension: /^\.((\j|\t)sx?)$/,
--- middleware/sass-compiler/index.ts
+++ middleware/sass-compiler/index.ts
@@ -1,64 +1,6 @@
 import * as sass from "sass";
 
-/**
- * A file interface for WikiPages.
- */
-interface WPFile {
-  /**
-   * The original directory of the file.
-   */
-  readonly originalDirectory: string;
-  /**
-   * The short extension of the file, ex `.lua`, not including `.client.lua`
-   */
-  shortExtension: string;
-  /**
-   * The long extension o fthe file, ex `.client.lua`, not including **just** `.lua`
-   */
-  longExtension: string;
-  /**
-   * The comment for the MediaWiki commit
-   */
-  commitComment: string;
-  /**
-   * Whether or not it should commit when it goes through all middlewares
-   */
-  shouldCommit: boolean;
-  /**
-   * THe new MediaWiki path of the file.
-   */
-  path: string;
-  /**
-   * The source string of the file.
-   */
-  source: string;
-}
-
-/**
- * Middleware that will be called on every file found.
- */
-interface Middleware {
-  /**
-   * The RegExp/string to match in the short extension,  ex `.lua`, not including `.client.lua`
-   */
-  matchShortExtension?: RegExp | string;
-  /**
-   * The RegExp/string to match in the long extension, ex `.client.lua`, not including **just** `.lua`
-   */
-  matchLongExtension?: RegExp | string;
-  /**
-   * The Regexp/string to match in the path of the file
-   */
-  matchPath?: RegExp | string;
-  /**
-   * The settings index to be indexed with
-   */
-  settingsIndex?: string;
-  /**
-   * The execute function of the middleware
-   */
-  execute: (file: WPFile, settings?: Record<string, unknown>) => WPFile
-}
+import type { Middleware } from "@rumblewikis/wikipages";
 
 const sassCompiler: Middleware = {
   matchShortExtension: /^\.s(c|a)ss$/,
